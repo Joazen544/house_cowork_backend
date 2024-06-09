2b14b8c869c2e86af3d0c9dc85047e3092c8c2ac
Joazen <johnsons544@gmail.com>
2024-06-09 13:53:27 +0800

    feat: basic CRUD for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(@Query('age') age?: string) {
+    const ageNumber = typeof age == 'string' ? parseInt(age) : undefined;
+
+    return this.usersService.getUsers(ageNumber);
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.usersService.getUser(+id);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.removeUser(+id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 0, name: 'Johnson', age: 25 },
+    { id: 1, name: 'Polly', age: 24 },
+  ];
+
+  getUsers(age?: number) {
+    if (age) {
+      return this.users.filter((user) => user.age == age);
+    }
+
+    return this.users;
+  }
+
+  getUser(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const lastUser = this.users[this.users.length - 1];
+    const lastId = lastUser.id;
+    const newUser = {
+      ...createUserDto,
+      id: lastId + 1,
+    };
+    this.users.push(newUser);
+    return this.users;
+  }
+
+  updateUser(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto };
+      }
+      return user;
+    });
+
+    return this.getUser(id);
+  }
+
+  removeUser(id: number) {
+    const removedTarget = this.getUser(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedTarget;
+  }
+}
