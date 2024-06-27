dcd30ebd42b6fcbe36c76ff392f3384387e063b5
Joazen <johnsons544@gmail.com>
2024-06-27 20:54:52 +0800

    feat: setup serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,13 +11,18 @@ import {
   UseGuards,
   ValidationPipe,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AdminGuard } from 'src/admin/admin.guard';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 
 @Controller('users')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -28,6 +33,7 @@ export class UsersController {
 
   @Get(':id')
   async findUser(@Param('id', ParseIntPipe) id: number) {
+    console.log('running in controller');
     const user = await this.usersService.findOne(id);
     if (!user) {
       throw new NotFoundException('user not found');
