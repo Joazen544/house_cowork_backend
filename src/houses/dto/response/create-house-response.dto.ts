e8e4fd6e4f3ff7de9689dff03f442f9c9852aad9
Joazen <johnsons544@gmail.com>
2024-07-25 20:41:30 +0800

    feat: add createHouseDto

--- src/houses/dto/response/create-house-response.dto.ts
+++ src/houses/dto/response/create-house-response.dto.ts
@@ -1,1 +1,17 @@
-export class CreateHouseResponseDto {}
+import { IsArray, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateHouseResponseDto {
+  @ApiProperty({ example: 'No 10 Floor 5' })
+  @IsString()
+  name!: string;
+
+  @ApiProperty({ example: 'This is a warm house' })
+  @IsString()
+  description!: string;
+
+  @ApiProperty({ example: ['Pay the rent on 5th', 'Be clean', 'Be happy', 'Dogs and cats are welcome'] })
+  @IsArray()
+  @IsString({ each: true })
+  rules!: string[];
+}
