1c47a0f26bf8f68d0b6d696a57acb0db829f521a
Joazen <johnsons544@gmail.com>
2024-08-15 23:29:54 +0800

    feat: add Rule entity to house module

--- src/resources/houses/houses.module.ts
+++ src/resources/houses/houses.module.ts
@@ -4,9 +4,10 @@ import { HousesController } from './houses.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { House } from './entities/house.entity';
 import { UsersModule } from 'src/resources/users/users.module';
+import { Rule } from './entities/rule.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([House]), UsersModule],
+  imports: [TypeOrmModule.forFeature([House, Rule]), UsersModule],
   controllers: [HousesController],
   providers: [HousesService],
   exports: [HousesService],
