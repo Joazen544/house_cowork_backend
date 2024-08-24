b5ed0bfa8f8845383482c21bfa2fbac3e67238dc
Joazen <johnsons544@gmail.com>
2024-08-24 14:43:32 +0800

    feat: make a decorator so dto can inherit from others

--- src/decorators/inherit-api-property.decorator.ts
+++ src/decorators/inherit-api-property.decorator.ts
@@ -1,16 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import 'reflect-metadata';
-
-function getPropertyDecorators(target: any, propertyKey: string) {
-  return Reflect.getMetadata('design:decorators', target, propertyKey) || [];
-}
 
 export function InheritApiProperty(sourceClass: any) {
-  return (target: any, propertyKey: string) => {
-    const decorators = getPropertyDecorators(sourceClass.prototype, propertyKey);
-    const apiPropertyDecorator = decorators.find((d: any) => d instanceof ApiProperty);
-    if (apiPropertyDecorator) {
-      ApiProperty(apiPropertyDecorator)(target, propertyKey);
+  return (target: any, key: string) => {
+    const metadata = Reflect.getMetadata('swagger/apiModelProperties', sourceClass.prototype, key);
+    if (metadata) {
+      ApiProperty(metadata)(target, key);
     }
   };
 }
