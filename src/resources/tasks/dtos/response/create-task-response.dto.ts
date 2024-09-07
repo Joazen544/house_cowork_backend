e053a81ffcd4b50f8e42fce4d189dda11484028f
Joazen <johnsons544@gmail.com>
2024-09-07 21:44:17 +0800

    feat: add todo to change response into id

--- src/resources/tasks/dtos/response/create-task-response.dto.ts
+++ src/resources/tasks/dtos/response/create-task-response.dto.ts
@@ -2,6 +2,7 @@ import { TaskDto } from '../task.dto';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskResponseDto {
+  // TODO: response all data as id
   @ApiProperty({ type: TaskDto })
   task!: TaskDto;
 }
