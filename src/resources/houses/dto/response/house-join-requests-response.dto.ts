60368c2bae6a800e02a3fd40f8f2d4f6648181d3
Joazen <johnsons544@gmail.com>
2024-09-07 20:47:22 +0800

    feat: change the join requests response

--- src/resources/houses/dto/response/house-join-requests-response.dto.ts
+++ src/resources/houses/dto/response/house-join-requests-response.dto.ts
@@ -1,11 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { HouseDto } from '../house.dto';
 import { JoinRequestDto } from '../join-request.dto';
 
 export class HouseJoinRequestsResponseDto {
-  @ApiProperty({ type: HouseDto })
-  house!: HouseDto;
-
-  @ApiProperty({ type: JoinRequestDto })
-  joinRequest!: JoinRequestDto;
+  @ApiProperty({ type: [JoinRequestDto] })
+  joinRequests!: JoinRequestDto[];
 }
