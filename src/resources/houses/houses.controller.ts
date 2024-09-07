cd9bf0d5d428a1b9874bf28ad478a86b4d7a8b8c
Joazen <johnsons544@gmail.com>
2024-09-07 20:57:53 +0800

    feat: remove delete from controller import since not used

--- src/resources/houses/houses.controller.ts
+++ src/resources/houses/houses.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Delete,
-  HttpCode,
-  HttpStatus,
-  Query,
-  UseGuards,
-  Param,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, HttpCode, HttpStatus, Query, UseGuards, Param } from '@nestjs/common';
 import { HousesService } from './houses.service';
 import { CreateHouseDto } from './dto/request/create-house.dto';
 import { UpdateHouseDto } from './dto/request/update-house.dto';
