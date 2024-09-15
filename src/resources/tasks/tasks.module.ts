0fd95d0e990642bd7129a79e0d032d4fcc70e3e7
Joazen <johnsons544@gmail.com>
2024-09-15 15:17:12 +0800

    feat: add repositories as module providers

--- src/resources/tasks/tasks.module.ts
+++ src/resources/tasks/tasks.module.ts
@@ -6,10 +6,12 @@ import { Task } from './entities/task.entity';
 import { UsersModule } from 'src/resources/users/users.module';
 import { HousesModule } from 'src/resources/houses/houses.module';
 import { TaskAssignment } from './entities/task-assignment.entity';
+import { TasksRepository } from './repositories/tasks.repository';
+import { TaskAssignmentsRepository } from './repositories/task-assignments.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task, TaskAssignment]), UsersModule, HousesModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository, TaskAssignmentsRepository],
 })
 export class TasksModule {}
