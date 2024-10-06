bf3a83d9570bd3ca0c323d5a0ef7071c2002e765
Joazen <johnsons544@gmail.com>
2024-10-06 17:14:02 +0800

    feat: change the invitationCode to code

--- src/modules/houses/dto/response/create-house-invitation-response.dto.ts
+++ src/modules/houses/dto/response/create-house-invitation-response.dto.ts
@@ -4,7 +4,7 @@ import { Expose } from 'class-transformer';
 class InvitationDto {
   @ApiProperty({ example: 'fdwghhf3' })
   @Expose()
-  invitationCode!: string;
+  code!: string;
 
   @ApiProperty({ example: '2024-01-01T00:00:00Z' })
   @Expose()
