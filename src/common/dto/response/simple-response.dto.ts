d498adc3b7c41c01defc2fab6c0687d6a627321e
Joazen <johnsons544@gmail.com>
2024-10-07 20:29:40 +0800

    feat: show the simple response

--- src/common/dto/response/simple-response.dto.ts
+++ src/common/dto/response/simple-response.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class SimpleResponseDto {
   @ApiProperty({ example: true })
+  @Expose()
   result!: boolean;
 }
