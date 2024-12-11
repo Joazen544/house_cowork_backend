5003cf843cc060afcc18ca9330edf0bcfed3d594
Joazen <johnsons544@gmail.com>
2024-12-11 23:09:16 +0800

    feat: download aws sdk to handle parameter store, and write a load-parameter js to create env

---
+++ load-parameter.js
@@ -0,0 +1,55 @@
+import AWS from 'aws-sdk';
+import fs from 'fs';
+
+const ssm = new AWS.SSM();
+
+async function loadParameter(name) {
+  try {
+    const prefix = '/house-cowork/';
+    const params = {
+      Name: prefix + name,
+      WithDecryption: true,
+    };
+    const data = await ssm.getParameter(params).promise();
+    return data.Parameter.Value;
+  } catch (error) {
+    console.error(`Error reading parameter: ${name}`, error);
+    throw error;
+  }
+}
+
+async function loadEnv() {
+  try {
+    if (fs.existsSync('.env.production')) {
+      fs.unlinkSync('.env.production');
+    }
+
+    const databaseType = await loadParameter('database-type');
+    const databaseHost = await loadParameter('database-host');
+    const databaseName = await loadParameter('database-name');
+    const databaseUsername = await loadParameter('database-username');
+    const databasePassword = await loadParameter('database-password');
+    const jwtSecret = await loadParameter('jwt-secret');
+
+    fs.appendFileSync(
+      '.env.production',
+      `
+      DATABASE_TYPE=${databaseType}\n
+      DATABASE_NAME=${databaseName}\n
+      DATABASE_HOST=${databaseHost}\n
+      DATABASE_PORT=5432\n
+      DATABASE_USERNAME=${databaseUsername}\n
+      DATABASE_PASSWORD=${databasePassword}\n
+      DATABASE_SSL=true\n
+      JWT_SECRET=${jwtSecret}\n
+    `,
+    );
+
+    console.log('Environment variables loaded and .env file created successfully.');
+  } catch (error) {
+    console.error('Error loading environment variables', error);
+    process.exit(1);
+  }
+}
+
+loadEnv();
