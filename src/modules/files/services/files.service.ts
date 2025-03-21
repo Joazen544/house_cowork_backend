6a871c49d8f87aa22906a5fcbd7773af51b575e8
Joazen <johnsons544@gmail.com>
2025-03-22 00:06:31 +0800

    add category in front of the key, so CDN can distinguish which bucket to use

--- src/modules/files/services/files.service.ts
+++ src/modules/files/services/files.service.ts
@@ -13,9 +13,7 @@ export class FilesService {
   ) {}
 
   async uploadFile(file: Express.Multer.File, category: FileCategory) {
-    const lastDotIndex = file.originalname.lastIndexOf('.');
-    const fileExt = lastDotIndex !== -1 ? file.originalname.substring(lastDotIndex + 1) : '';
-    const originalName = lastDotIndex !== -1 ? file.originalname.substring(0, lastDotIndex) : file.originalname;
+    const { fileExt, originalName } = this.getFileExtensionAndOriginalName(file);
 
     if (!fileExt) {
       throw new BadRequestException('Invalid file extension!');
@@ -27,7 +25,14 @@ export class FilesService {
 
     await this.fileStorageService.uploadFile(file.buffer, bucketName, key, file.mimetype);
 
-    return fileName;
+    return this.getUrl(category, fileName);
+  }
+
+  private getFileExtensionAndOriginalName(file: Express.Multer.File) {
+    const lastDotIndex = file.originalname.lastIndexOf('.');
+    const fileExt = lastDotIndex !== -1 ? file.originalname.substring(lastDotIndex + 1) : '';
+    const originalName = lastDotIndex !== -1 ? file.originalname.substring(0, lastDotIndex) : file.originalname;
+    return { fileExt, originalName };
   }
 
   private generateFileName(category: FileCategory, originalName: string, fileExt: string): string {
@@ -41,7 +46,7 @@ export class FilesService {
     }
   }
 
-  getUrl(category: FileCategory, fileName: string) {
+  private getUrl(category: FileCategory, fileName: string) {
     const key = this.getFileKey(category, fileName);
     return `https://d1px1ztgevq7tr.cloudfront.net/${key}`;
   }
@@ -51,11 +56,16 @@ export class FilesService {
   }
 
   private getFileKey(category: FileCategory, fileName: string) {
+    let suffix: string;
     switch (category) {
       case FileCategory.USER_AVATAR:
-        return fileName;
       case FileCategory.HOUSE_AVATAR:
-        return fileName;
+        suffix = fileName;
+        break;
+      default:
+        throw new BadRequestException('Invalid file category!');
     }
+
+    return `${category}/${suffix}`;
   }
 }
