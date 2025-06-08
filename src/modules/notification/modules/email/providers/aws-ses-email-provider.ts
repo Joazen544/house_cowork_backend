91273beb2390238ffeccc1ed26e32faa4a990625
Joazen <johnsons544@gmail.com>
2025-06-08 15:02:00 +0800

    change aws package to new one

--- src/modules/notification/modules/email/providers/aws-ses-email-provider.ts
+++ src/modules/notification/modules/email/providers/aws-ses-email-provider.ts
@@ -1,34 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { EmailProvider } from './interfaces/email-provider.interface';
 import { EmailSendOptions } from '../dtos/email-send-options.dto';
-import * as AWS from 'aws-sdk';
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 import { EmailSendResult } from '../interfaces/email-send-result.interface';
 import { EmailProvider as EmailProviderEnum } from './enums/email-provider.enum';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AwsSesEmailProvider implements EmailProvider {
-  private ses = new AWS.SES({ region: process.env.AWS_REGION });
+  private readonly ses: SESClient;
+
+  constructor(
+    @Inject(ConfigService)
+    private readonly configService: ConfigService,
+  ) {
+    const region = this.configService.get<string>('AWS_REGION');
+    const accessKeyId = this.configService.get<string>('AWS_ACCESS_KEY_ID');
+    const secretAccessKey = this.configService.get<string>('AWS_SECRET_ACCESS_KEY');
+
+    if (!region || !accessKeyId || !secretAccessKey) {
+      throw new Error('Missing AWS configuration');
+    }
+
+    this.ses = new SESClient({
+      region,
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+    });
+  }
 
   async sendEmail(options: EmailSendOptions): Promise<EmailSendResult> {
     const params = {
       Source: options.from,
-      Destination: { ToAddresses: [options.to] },
+      Destination: {
+        ToAddresses: [options.to],
+      },
       Message: {
-        Subject: { Data: options.subject },
+        Subject: {
+          Data: options.subject,
+        },
         Body: {
-          Html: { Data: options.htmlBody },
-          Text: { Data: options.textBody },
+          Html: {
+            Data: options.htmlBody,
+          },
+          Text: {
+            Data: options.textBody,
+          },
         },
       },
     };
 
-    const response = await this.ses.sendEmail(params).promise();
-    const isSuccess = response.MessageId !== undefined;
-    return {
-      success: isSuccess,
-      messageId: response.MessageId,
-      provider: EmailProviderEnum.AWS_SES,
-      sendAt: new Date(),
-    };
+    try {
+      const command = new SendEmailCommand(params);
+      const response = await this.ses.send(command);
+      const isSuccess = response.MessageId !== undefined;
+      return {
+        success: isSuccess,
+        messageId: response.MessageId,
+        provider: EmailProviderEnum.AWS_SES,
+        sendAt: new Date(),
+      };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`Failed to send email: ${error.message}`);
+      }
+      throw new Error('Failed to send email: Unknown error');
+    }
   }
 }
